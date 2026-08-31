22a54abe7096abd0aed98158362931839d35d29d
Dasika Sarath <sarathdasika@gmail.com>
2026-08-31 20:20:05 +0530

    hi

--- src/main/java/ec/example/sheprenure/Entity/UserEntity.java
+++ src/main/java/ec/example/sheprenure/Entity/UserEntity.java
@@ -13,11 +13,8 @@ import lombok.Data;
 @Data
 public class UserEntity {
 
-
-
-    //variables
     @Id
-    @GeneratedValue(strategy =GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int userId;
     private String name;
@@ -32,28 +29,4 @@ public class UserEntity {
     private String role;
     private Boolean isVerified = false;
 
- /*    //constructors(7-saltilu)
-    public UserEntity(){
-
-    }
-
-    //getters
-    public int getUserid(){return userid;}
-    public String getName(){return name;}
-    public String getEmail(){return email;}
-    public String getShippingAdd(){return shippingAdd;}
-    public String getPassword(){return password;}
-    public String getPincode(){return pincode;}
-    public String getMobile(){return mobile;}
-
-
-    //setters
-    public void setUserid(int user_id){this.userid=userid;}
-    public void setName(String name){this.name=name;}
-    public void setEmail(String email){this.email=email;}
-    public void setShippingAdd(String shippingAdd){this.shippingAdd=shippingAdd;}
-    public void setPassword(String password){this.password=password;}
-    public void setPincode(String pincode){this.pincode=pincode;}
-    public void setMobile(String mobile){this.mobile=mobile;}
-    */
 }
